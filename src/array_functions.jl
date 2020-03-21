964e08f7d14c40a285eea951c1029282995bef3b
Hayate Nakano <nakanodesu@gmail.com>
2020-03-21 20:37:47 +0900

    make tensor_to_matrix and matrix_to_tensor private

--- src/array_functions.jl
+++ src/array_functions.jl
@@ -3,25 +3,25 @@ function similar_normalized_eye(C::T) where {T<:AbstractMatrix}
     T(Diagonal(I / sqrt(size(C, 1)), size(C, 1)))
 end
 
-function tensor_to_matrix(X::AbstractTensor3, lp_r::Bool = true)
+function _tensor_to_matrix(X::AbstractTensor3, lp_r::Bool = true)
     d = size(X, 1)
     lp_r ? reshape(permutedims(X, (1, 3, 2)), :, d) : reshape(X, d, :)
 end
 
-function matrix_to_tensor(X::AbstractMatrix, lp_r::Bool = true)
+function _matrix_to_tensor(X::AbstractMatrix, lp_r::Bool = true)
     d = lp_r ? size(X, 2) : size(X, 1)
     lp_r ? permutedims(reshape(X, d, :, d), (1, 3, 2)) : reshape(X, d, d, :)
 end
 
 function al_and_ar(AC::AbstractTensor3, C::AbstractMatrix)
     AC_Cdag = mul_matrix_from_right(AC, C')
-    x, y, z = svd(tensor_to_matrix(AC_Cdag, true))
-    AL = matrix_to_tensor(x * z', true)
+    x, y, z = svd(_tensor_to_matrix(AC_Cdag, true))
+    AL = _matrix_to_tensor(x * z', true)
     ϵL = norm(AC - mul_matrix_from_right(AL, C))
 
     Cdag_AC = mul_matrix_from_left(AC, C')
-    x, y, z = svd(tensor_to_matrix(AC_Cdag, false))
-    AR = matrix_to_tensor(x * z', false)
+    x, y, z = svd(_tensor_to_matrix(AC_Cdag, false))
+    AR = _matrix_to_tensor(x * z', false)
     ϵR = norm(AC - mul_matrix_from_left(AR, C))
 
     (AL, AR), (ϵL, ϵR)
